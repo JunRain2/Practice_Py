b8c360308c039b5e76c2599d6b71be18e6ce7214
JunRain2 <sad3101@outlook.kr>
2022-07-16 16:07:59 +0900

    CHAP03

---
+++ chap03/chap03-01.py
@@ -0,0 +1,9 @@
+x = int(input("x : "))
+y = int(input("y : "))
+
+print("두수의 합 :", x+y)
+print("두수의 차 :", x-y)
+print("두수의 곱 :", x*y)
+print("두수의 평균 :", (x+y)/2)
+print("큰수 :", max(x,y))#max(x,y)두 수 중 큰수 선택
+print("작은 수 :", min(x,y))# min(x,y)작은 수 선택
